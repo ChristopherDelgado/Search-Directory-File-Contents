c20ea09fe1cfd10391a0bfd7e3765c2bf7ad72dc
Christopher <Christopher.delgado.754@my.csun.edu>
2020-09-30 17:01:18 -0700

    Reduced the amount of branches by merging similiar code inside of get_instances()

--- directory_search.py
+++ directory_search.py
@@ -3,7 +3,7 @@ import os
 import docx
 
 
-def search_directory(dir, filetype, search, specific=1) -> list:
+def search_directory(dir, filetype, search) -> list:
     """Search files in the given directory with the given file extension for the desired search criteria"""
     # change directory to the given directory
     os.chdir(dir)
@@ -15,8 +15,8 @@ def search_directory(dir, filetype, search, specific=1) -> list:
         for file in os.listdir(os.getcwd()):
             if file.endswith(filetype):
                 # retrieve all instances of the search criteria in the file and append to list
-                temp_list = get_instances(file, search, specific)
-                if temp_list is not None:
+                temp_list = get_instances(file, search)
+                if temp_list is not None and len(temp_list) > 1:
                     files_with_search.append(temp_list)
     except Exception as e:
         raise e
@@ -24,7 +24,7 @@ def search_directory(dir, filetype, search, specific=1) -> list:
     return files_with_search
 
 
-def get_instances(file, search, specific) -> list:
+def get_instances(file, search) -> list:
     """Return all instances of the search criteria found in the given file in a list where [0] is the file's name"""
     # save the files name before modifying it into a read file
     file_name = file
@@ -47,10 +47,16 @@ def get_instances(file, search, specific) -> list:
     # check if the file holds the desired search criteria
     if file.__contains__(search):
         this_file_todo = [file_name]
-        if specific == 0:
-            while file.__contains__(search):
-                # store index where search is found
-                start_index = file.find(search)
+        while file.__contains__(search):
+            # store index where search is found
+            start_index = file.find(search)
+            # making sure result is exact match
+            temp_end_index = len(search) + start_index
+            # make the temp result take the character before
+            #     the start index and after the end index
+            #     to check for exact match
+            temp_result = file[start_index - 1:temp_end_index + 1]
+            if search.__eq__(temp_result.strip()):
                 # update file seek location
                 temp_file = file[start_index:-1]
                 # store newline character index
@@ -61,37 +67,17 @@ def get_instances(file, search, specific) -> list:
                 # append the search to the list and update file seek
                 this_file_todo.append('...' + file[start_index:end_index] + '...')
                 file = file[end_index:-1]
-            return this_file_todo
-        else:
-            while file.__contains__(search):
-                # store index where search is found
-                start_index = file.find(search)
-                # making sure result is exact match
-                temp_exact_file = file[start_index:-1]
-                temp_end_index = temp_exact_file.find(' ') + start_index
-                temp_result = file[start_index:temp_end_index]
-                if search.__eq__(temp_result):
-                    # update file seek location
-                    temp_file = file[start_index:-1]
-                    # store newline character index
-                    end_index = temp_file.find(end_character)
-                    # if end_index is found as a positive integer add the start index to it
-                    if end_index >= 0:
-                        end_index += start_index
-                    # append the search to the list and update file seek
-                    this_file_todo.append('...' + file[start_index:end_index] + '...')
-                    file = file[end_index:-1]
-                else:
-                    file = file[temp_end_index:-1]
+            else:
+                file = file[temp_end_index:-1]
             return this_file_todo
 
     return None
 
 
-def print_results(double_list, search):
+def print_results(returned_list, search):
     """Print all files that contain the search criteria as well as what was found"""
-    input('%d Files found containing "%s" press ENTER for first file\n' % (len(double_list), search))
-    for list in double_list:
+    input('%d Files found containing "%s" press ENTER for first file\n' % (len(returned_list), search))
+    for list in returned_list:
         for index in range(0, len(list)):
             print(list[index])
         input('\npress ENTER for the next file\n')
@@ -102,14 +88,14 @@ def start():
     # run the program until the user quits
     while True:
         # retrieve input from the user
-        m_input = input("Enter directory, .file extension, search, and specific in that format\n")
+        m_input = input("Enter directory, .file extension, search in that format\n")
         # split the input into a list
         m_input = m_input.split(',')
         # remove any leading and trailing white spaces from all inputs
         for i in range(0, len(m_input)):
             m_input[i] = m_input[i].strip()
         # retrieve the files and print the data
-        print_results(search_directory(m_input[0], m_input[1], m_input[2], m_input[3]), m_input[2])
+        print_results(search_directory(m_input[0], m_input[1], m_input[2]), m_input[2])
         # ask if the user would like to search another directory
         m_option = input('Would you like to search another directory?\n')
         if m_option.lower().__eq__('yes'):
